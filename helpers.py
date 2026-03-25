89f1c3b6a620e7afb4bd100f2efc80d3423eebac
YvamDarc <88706763+YvamDarc@users.noreply.github.com>
2026-03-25 11:42:43 +0100

    Delete helpers.py

--- helpers.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.title("Page 4 - Consolidation future")
-st.info(
-    "Ici on branchera ensuite les autres tableaux : Lancelot, frais/km, variables individuelles enrichies, puis le récap Silae pour ajouter les matricules et sortir l'export final."
-)
